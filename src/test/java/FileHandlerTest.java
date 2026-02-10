3210ef72e6ae43c9854bbfbd6a7a21c4a9d83ca5
Aarush <penmetsa.aarush@gmail.com>
2026-02-10 14:07:28 -0500

    create test files

--- src/test/java/FileHandlerTest.java
+++ src/test/java/FileHandlerTest.java
@@ -1,4 +1,26 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FileHandlerTest {
-  
+    private FileHandler fileHandler;
+
+    @BeforeEach
+    void setUp() {
+        fileHandler = new FileHandler();
+    }
+
+    @Test
+    void getFileList() {
+
+    }
+
+    @Test
+    void readFile() {
+    }
+
+    @Test
+    void testReadFile() {
+    }
 }
