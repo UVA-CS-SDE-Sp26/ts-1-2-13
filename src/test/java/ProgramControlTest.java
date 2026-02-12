6f33f4452a1a58b42f0c0cde1af2c131dc6c37db
Mackenzie Allen <ppj9ny@virginia.edu>
2026-02-12 16:58:05 -0500

    tests

--- src/test/java/ProgramControlTest.java
+++ src/test/java/ProgramControlTest.java
@@ -3,8 +3,20 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Scanner;
 
 public class ProgramControlTest {
+    private final Scanner scanner;
+
+    public ProgramControlTest(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public ProgramControlTest() {
+        this(new Scanner(System.in));
+    }
+
+
 
     @BeforeEach
     void setUp() {
